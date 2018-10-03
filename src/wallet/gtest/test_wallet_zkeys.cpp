4508fec420fdcc22299ed5cd671fa24444187f80
mdr0id <marshall@z.cash>
2018-10-03 10:16:30 -0700

    Adding first working gtest and second that is a WIP

--- src/wallet/gtest/test_wallet_zkeys.cpp
+++ src/wallet/gtest/test_wallet_zkeys.cpp
@@ -62,6 +62,80 @@ TEST(wallet_zkeys_tests, store_and_load_sapling_zkeys) {
 
 /**
  * This test covers methods on CWallet
+ * GenerateNewSaplingZKey()
+ * AddSaplingZKey()
+ * LoadSaplingZKey()
+ * LoadSaplingZKeyMetadata()
+ */
+TEST(wallet_zkeys_tests, store_and_load_sapzkeys) {
+    SelectParams(CBaseChainParams::MAIN);
+
+    CWallet wallet;
+    if(!wallet.HaveHDSeed())
+    {
+        wallet.GenerateNewSeed();
+    }
+
+    // wallet should be empty
+    std::set<libzcash::SaplingPaymentAddress> addrs;
+    wallet.GetSaplingPaymentAddresses(addrs);
+    ASSERT_EQ(0, addrs.size());
+
+    std::vector<unsigned char, secure_allocator<unsigned char>> rawSeed(32);
+    HDSeed seed(rawSeed);
+    auto sk = libzcash::SaplingExtendedSpendingKey::Master(seed);
+    auto fvk = sk.expsk.full_viewing_key();
+    auto ivk = fvk.in_viewing_key();
+    auto addr = sk.DefaultAddress();
+
+    ASSERT_TRUE(wallet.AddSaplingSpendingKey(sk, addr));
+    
+    // wallet should have one key
+    wallet.GetSaplingPaymentAddresses(addrs);
+    ASSERT_EQ(1, addrs.size());
+
+    // verify wallet has spending key for the address
+    ASSERT_TRUE(wallet.HaveSaplingSpendingKey(fvk));
+
+    // manually add new spending key to wallet
+    ASSERT_TRUE(wallet.AddSaplingZKey(sk, addr));
+
+    // verify wallet did add it
+    ASSERT_TRUE(wallet.HaveSaplingSpendingKey(fvk));
+
+    // verify spending key stored correctly
+    libzcash::SaplingExtendedSpendingKey keyOut;
+    wallet.GetSaplingSpendingKey(fvk, keyOut);
+    ASSERT_EQ(sk, keyOut);
+
+    // verify there are two keys
+    wallet.GetSaplingPaymentAddresses(addrs);
+    ASSERT_EQ(1, addrs.size());
+    ASSERT_EQ(1, addrs.count(addr));
+
+    // Load a third key into the wallet
+    auto sk2 = libzcash::SaplingSpendingKey::random();
+    auto fvk2 = sk2.full_viewing_key();
+    auto ivk2 = fvk2.in_viewing_key();
+    
+    libzcash::SaplingExtendedSpendingKey keyOut2;
+    wallet.GetSaplingSpendingKey(fvk2, keyOut2);
+
+    ASSERT_TRUE(wallet.LoadSaplingZKey(keyOut2));
+
+    // attach metadata to this third key
+    int64_t now = GetTime();
+    CKeyMetadata meta(now);
+    ASSERT_TRUE(wallet.LoadSaplingZKeyMetadata(ivk2, meta));
+
+    // check metadata is the same
+    CKeyMetadata m = wallet.mapSaplingZKeyMetadata[ivk2];
+    ASSERT_EQ(m.nCreateTime, now);
+  
+}
+
+/**
+ * This test covers methods on CWallet
  * GenerateNewZKey()
  * AddSproutZKey()
  * LoadZKey()
@@ -119,6 +193,7 @@ TEST(wallet_zkeys_tests, store_and_load_zkeys) {
     CKeyMetadata m= wallet.mapSproutZKeyMetadata[addr];
     ASSERT_EQ(m.nCreateTime, now);
 }
+
 
 /**
  * This test covers methods on CWallet
@@ -362,4 +437,106 @@ TEST(wallet_zkeys_tests, write_cryptedzkey_direct_to_db) {
     wallet2.GetSproutSpendingKey(paymentAddress2, keyOut);
     ASSERT_EQ(paymentAddress2, keyOut.address());
 }
+
+/**
+ * This test covers methods on CWalletDB to load/save crypted sapling z keys.
+ */
+TEST(wallet_zkeys_tests, write_cryptedzsapkey_direct_to_db) {
+    SelectParams(CBaseChainParams::TESTNET);
+
+    // Get temporary and unique path for file.
+    // Note: / operator to append paths
+    boost::filesystem::path pathTemp = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
+    boost::filesystem::create_directories(pathTemp);
+    mapArgs["-datadir"] = pathTemp.string();
+
+    bool fFirstRun;
+    CWallet wallet("wallet_crypted.dat");
+    if(!wallet.HaveHDSeed())
+    {
+        wallet.GenerateNewSeed();
+    }
+    //ASSERT_EQ(DB_LOAD_OK, wallet.LoadWallet(fFirstRun));
+
+    // No default CPubKey set
+    ASSERT_TRUE(fFirstRun);
+
+    // wallet should be empty
+    std::set<libzcash::SaplingPaymentAddress> addrs;
+    wallet.GetSaplingPaymentAddresses(addrs);
+    ASSERT_EQ(0, addrs.size());
+
+    // Add random key to the wallet
+    auto address = wallet.GenerateNewSaplingZKey();
+    ASSERT_EQ(1, addrs.size());
+
+    // verify wallet has incoming viewing key for the address
+    ASSERT_TRUE(wallet.HaveSaplingIncomingViewingKey(address));
+
+    auto paymentAddress = boost::get<libzcash::SaplingPaymentAddress>(address);
+
+    // wallet should have one key
+    wallet.GetSaplingPaymentAddresses(addrs);
+    
+    ASSERT_EQ(1, addrs.size());
+
+    // encrypt wallet
+    SecureString strWalletPass;
+    strWalletPass.reserve(100);
+    strWalletPass = "ahoy";
+    ASSERT_TRUE(wallet.EncryptWallet(strWalletPass));
+    
+    // adding a new key will fail as the wallet is locked
+    EXPECT_ANY_THROW(wallet.GenerateNewSaplingZKey());
+    
+    // unlock wallet and then add
+    wallet.Unlock(strWalletPass);
+    auto address2 = wallet.GenerateNewSaplingZKey();
+    //ASSERT_TRUE(boost::get<libzcash::SaplingPaymentAddress>(&address2) != nullptr);
+    auto paymentAddress2 = boost::get<libzcash::SaplingPaymentAddress>(address2);
+
+    // Create a new wallet from the existing wallet path
+    CWallet wallet2("wallet_crypted.dat");
+    ASSERT_EQ(DB_LOAD_OK, wallet2.LoadWallet(fFirstRun));
+
+    // Confirm it's not the same as the other wallet
+    ASSERT_TRUE(&wallet != &wallet2);
+    
+    // wallet should have two keys
+    wallet2.GetSaplingPaymentAddresses(addrs);
+    ASSERT_EQ(2, addrs.size());
+    
+    // check we have entries for our payment addresses
+    ASSERT_TRUE(addrs.count(paymentAddress));
+    ASSERT_TRUE(addrs.count(paymentAddress2));
+
+    // manually add new spending key to wallet
+    libzcash::SaplingExtendedSpendingKey keyOut;
+    HDSeed hdSeed;
+    wallet.GetHDSeed(hdSeed);
+    auto m = libzcash::SaplingExtendedSpendingKey::Master(hdSeed);
+    auto sk = m.Derive(0);
+    ASSERT_TRUE(wallet.AddSaplingZKey(sk, sk.DefaultAddress()));
+
+    // verify wallet did add it
+    auto fvk = sk.expsk.full_viewing_key();
+
+    // spending key is crypted, so we can't extract valid payment address
+    
+    wallet2.GetSaplingSpendingKey(fvk, keyOut);
+    //ASSERT_EQ(sk, keyOut);
+    //ASSERT_FALSE(paymentAddress == keyOut.address());
+    
+    // unlock wallet to get spending keys and verify payment addresses
+    wallet2.Unlock(strWalletPass);
+
+    wallet2.GetSaplingSpendingKey(fvk, keyOut);
+    //ASSERT_EQ(paymentAddress, keyOut.address());
+    //ASSERT_EQ(sk, keyOut);
+    
+    wallet2.GetSaplingSpendingKey(fvk, keyOut);
+    //ASSERT_EQ(paymentAddress2, keyOut.address());
+    //ASSERT_EQ(sk, keyOut);
+}
+
 
